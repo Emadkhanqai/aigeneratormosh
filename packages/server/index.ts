6eb2a048779eea682907daef329070f19b4dd239
Emad <emad@hayya.qa>
2025-09-30 16:46:04 +0500

    front and backend connected

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send(process.env.OPENAI_API_KEY || "No API Key Set");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({
+    'message':'Hello from the backend 2!'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
